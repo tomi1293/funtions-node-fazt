0ece2f5d8faa3ce0a3c36ef49a546687ade08635
Tomas Martinez <martineztomi93@gmail.com>
2023-10-30 09:59:22 -0300

    Correcciones para pasar el lint

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,26 @@
+/* eslint-disable linebreak-style */
 const {onRequest} = require("firebase-functions/v2/https");
+const {applicationDefault} = require("firebase-admin/app");
 
-const express = require('express');
-const cors = require("cors");
+const admin = require("firebase-admin");
 
-//*Inicializamos express
+// Damos permiso a admin
+admin.initializeApp({
+  credential: applicationDefault(),
+});
+
+
+const express = require("express");
+
+// *Inicializamos express
 const app = express();
 // app.use(cors)
 
-app.get('/hello-world', ( req, res ) => {
-    return res.status(200).json({message: 'Hello World'})
+app.get("/hello-world", ( req, res ) => {
+  return res.status(200).json({message: "Hello World"});
 });
 
-app.use(require('./routes/products.routes'))
+app.use(require("./routes/products.routes"));
 
-//* DE ESTA FORMA ESTAMOS SIRVIENDO A FIREBASE CON NUESTRO BACKEND
+// *DE ESTA FORMA ESTAMOS SIRVIENDO A FIREBASE CON NUESTRO BACKEND
 exports.app = onRequest(app);
--- functions/routes/products.routes.js
+++ functions/routes/products.routes.js
@@ -1,92 +1,81 @@
-const {Router} = require('express');
+/* eslint-disable linebreak-style */
+/* eslint-disable new-cap */
+const {Router} = require("express");
 const router = Router();
-const { getFirestore } = require("firebase-admin/firestore");
-const { applicationDefault } = require("firebase-admin/app");
-const admin = require("firebase-admin");
-
-//Damos permiso a admin
-admin.initializeApp({
-    credential: applicationDefault(),
-})
-
+const {getFirestore} = require("firebase-admin/firestore");
 const db = getFirestore();
-    
 
-//! AGREGAMOS UN PRODUCTO 
-router.post('/api/products', async( req, res ) => {
-    try {
-        await db.collection('products')
+// !AGREGAMOS UN PRODUCTO
+router.post("/api/products", async (req, res) => {
+  try {
+    await db.collection("products")
         .doc(`/${req.body.id}/`)
-        .create({name:req.body.name});
+        .create({name: req.body.name});
 
-        return res.status(204).json();        
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
-    }
+    return res.status(204).json();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
 
-})
+// !TRAEMOS UN SOLO PRODUCTO POR SU ID
+router.get("/api/products/:product_id", async (req, res) => {
+  try {
+    const doc = db.collection("products").doc(req.params.product_id);
+    const item = await doc.get();
+    const response = item.data();
+    return res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
 
-//! TRAEMOS UN SOLO PRODUCTO POR SU ID
-router.get('/api/products/:product_id', async( req, res ) => {
-    try {
-        const doc = db.collection('products').doc(req.params.product_id)
-        const item = await doc.get();
-        const response =item.data();
-        return res.status(200).json(response);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
-    }
-})
+// ! TRAEMOS UN ARRAY CON TODOS LOS PRODUCTOS
+router.get("/api/products", async (req, res) => {
+  try {
+    const query = db.collection("products");
+    const querySnapshot = await query.get();
 
-//! TRAEMOS UN ARRAY CON TODOS LOS PRODUCTOS
-router.get('/api/products', async( req, res ) => {
-    try {     
-        const query = db.collection("products");
-        const querySnapshot = await query.get();
+    const response = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      name: doc.data().name,
+    }));
 
-        const response = querySnapshot.docs.map( doc => ({
-            id: doc.id,
-            name: doc.data().name
-        }))
+    return res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
 
-        return res.status(200).json(response);
-    
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
-    }
-})
+// !ELIMINAMOS UN PRODUCTO
+router.delete("/api/products/:product_id", async (req, res) => {
+  try {
+    const document = db.collection("products").doc(req.params.product_id);
+    await document.delete();
 
-//! ELIMINAMOS UN PRODUCTO
-router.delete('/api/products/:product_id', async( req, res ) => {
-    try {
-        const document = db.collection('products').doc(req.params.product_id);
-        await document.delete();
+    return res.status(200).json();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
 
-        return res.status(200).json();
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
-    }
-})
+// ! ACTUALIZAMOS UN PRODUCTO
+router.put("/api/products/:product_id", async (req, res) => {
+  try {
+    const document = db.collection("products").doc(req.params.product_id);
+    await document.update({
+      name: req.body.name,
+    });
 
-//! ACTUALIZAMOS UN PRODUCTO
-router.put('/api/products/:product_id', async( req, res ) => {
-    try {
-        const document = db.collection('products').doc(req.params.product_id);
-        await document.update({
-            name: req.body.name
-        })
+    return res.status(200).json();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
 
-        return res.status(200).json();
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
-    }
-})
-
-
-module.exports = router
+module.exports = router;
